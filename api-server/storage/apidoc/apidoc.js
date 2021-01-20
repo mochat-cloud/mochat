45bc3b5cd3f33b6d6b95c445389b5fd53762afda
liuxiufang <liuxiufang595@emao.com>
2021-01-20 12:48:56 +0800

    [代码完善](master): 成员统计查询字段完善&apidoc的url更换
    成员统计查询字段完善&apidoc的url更换

--- api-server/storage/apidoc/apidoc.js
+++ api-server/storage/apidoc/apidoc.js
@@ -4,7 +4,7 @@
         "version": "0.1.0",
         "description": "MoChat文档",
         "title": "MoChat",
-        "url": "http://chat.emao.com",
+        "url": "http://mo.chat/",
         "order": [
             "公共说明", "RequestCommon", "ResponseCommon", "PostUserAuth", "ResponsePage",
             "查看", "列表", "添加", "修改", "删除",
