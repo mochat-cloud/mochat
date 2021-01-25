f2b96bf4636048a4248b38ddb67ba6b995b111a3
fenglinhai <cuizhiwei1310@emao.com>
2021-01-25 17:37:37 +0800

    清缓存

--- dashboard/src/api/passWordUpdate.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function passWordUpdate (params) {
-  return request({
-    url: '/user/passwordUpdate',
-    method: 'put',
-    data: params
-  })
-}
