71b49977c1583526a17c6094f5aa8b26e9710057
wangpeiyan <wangpeiyan732@emao.com>
2021-02-03 15:46:25 +0800

    [新增功能](1.x): 新增添加应用接口

---
+++ api-server/app/Task/WorkAgent/SynWorkAgent.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of MoChat.
+ * @link     https://mo.chat
+ * @document https://mochat.wiki
+ * @contact  group@mo.chat
+ * @license  https://github.com/mochat-cloud/mochat/blob/master/LICENSE
+ */
+namespace App\Task\WorkAgent;
+
+use App\Contract\WorkAgentServiceInterface;
+use Hyperf\Crontab\Annotation\Crontab;
+use Hyperf\Di\Annotation\Inject;
+use Psr\SimpleCache\CacheInterface;
+use App\Logic\Agent\StoreLogic;
+
+/**
+ * @Crontab(name="pullAgent", rule="*\/30 * * * * *", callback="execute", singleton=true, memo="企业微信应用-同步应用")
+ */
+class SynWorkAgent
+{
+
+    /**
+     * @Inject
+     * @var WorkAgentServiceInterface
+     */
+    protected $workAgentClient;
+
+    /**
+     * @Inject
+     * @var StoreLogic
+     */
+    protected $storeLogic;
+
+    /**
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function execute(): void
+    {
+       $this->storeLogic->updateAgents();
+    }
+}
