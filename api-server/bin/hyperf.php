61db80b3fc2514677429e0da4726e6cde227a299
马新才 <maxincai@126.com>
2021-08-11 12:48:03 +0800

    [代码优化](master): 初始化时区

--- api-server/bin/hyperf.php
+++ api-server/bin/hyperf.php
@@ -6,6 +6,7 @@ ini_set('display_startup_errors', 'on');
 ini_set('memory_limit', '1G');
 
 error_reporting(E_ALL);
+date_default_timezone_set('Asia/Shanghai');
 
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 ! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL | SWOOLE_HOOK_CURL);
