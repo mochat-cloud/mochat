7a66ff300bcfc98196bce246b391eda2da75bab2
Czy <277171229@qq.com>
2021-08-24 11:08:14 +0800

    新增又拍云上传静态文件

--- operation/src/router/index.js
+++ operation/src/router/index.js
@@ -95,7 +95,7 @@ const routes = [
 ]
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  history: createWebHistory('/'),
   routes
 })
 
--- operation/vue.config.js
+++ operation/vue.config.js
@@ -1,3 +1,7 @@
+const env = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'preview'
+
 module.exports = {
-    publicPath: "./"
+    publicPath: env
+    ? 'https://assets-gewu.bagrids.com/operation_images/'
+    : '/',
 }
