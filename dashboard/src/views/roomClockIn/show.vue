127e49b5a2176be897ba0b39172a8e4f44ea3083
马新才 <maxincai@126.com>
2021-08-31 12:31:23 +0800

    [Bug修复](master): 修复群打卡接口方法导入名称错误

--- dashboard/src/views/roomClockIn/show.vue
+++ dashboard/src/views/roomClockIn/show.vue
@@ -184,7 +184,7 @@
 
 <script>
 // eslint-disable-next-line no-unused-vars
-import { detailsList, clientDetails, clockInDays, batchContactTagsApi } from '@/api/roomClockIn'
+import { detailsList, clientDetails, roomClockInDays, batchContactTagsApi } from '@/api/roomClockIn'
 import addlableIndex from '@/components/addlabel/index'
 import { Calendar } from 'ant-design-vue'
 export default {
@@ -352,7 +352,7 @@ export default {
     },
     // 查看行详情
     seeTableRow (obj) {
-      clockInDays({
+      roomClockInDays({
         id: this.activityId,
         contact_id: obj.id
       }).then((res) => {
