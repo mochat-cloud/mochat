fb36e6152122673f8a0980494867c4929b29a596
=baichonghua <18211167516@163.com>
2022-11-10 18:25:13 +0800

    debug

--- api-server/app/core/work-department/src/Logic/SyncLogic.php
+++ api-server/app/core/work-department/src/Logic/SyncLogic.php
@@ -54,6 +54,7 @@ class SyncLogic
         $this->client = make(WeWork::class);
         $this->workDepartmentService = make(WorkDepartmentContract::class);
         $corpData = $this->getCorpData($corpIds);
+        dump($corpData);
         if (empty($corpData)) {
             $this->logger->error('WorkDepartmentSynLogic->handle同步部门corp不能为空');
             return [];
