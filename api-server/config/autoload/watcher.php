34120a9436645ae3477b6774067f45e0380e7f94
bobofive <459938962@qq.com>
2022-12-07 16:39:38 +0800

    修改配置-热重启监听plugin目录

--- api-server/config/autoload/watcher.php
+++ api-server/config/autoload/watcher.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  */
 use Hyperf\Watcher\Driver\ScanFileDriver;
 
-$dir = ['app', 'config'];
+$dir = ['app', 'plugin', 'config'];
 
 return [
     'driver' => ScanFileDriver::class,
